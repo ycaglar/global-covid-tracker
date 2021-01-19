e04cd43311cedea9ec62fff0fd987d7a7ce6b4d1
Caglar <>
2021-01-19 16:37:22 -0600

    Update hover data

--- global_choropleth.py
+++ global_choropleth.py
@@ -29,7 +29,7 @@ fig = px.choropleth(df,
                     locations='Country_code',
                     color="New_cases",
                     hover_name="Country",
-                    hover_data=df.columns,
+                    hover_data=['Region', 'New_cases', 'Cumulative_cases', 'Cumulative_deaths'],
                     color_continuous_scale=px.colors.sequential.Plasma)
 fig.update_geos(projection_type="orthographic")
 fig.update_layout(height=500, margin={"r":0,"t":10,"l":0,"b":10})

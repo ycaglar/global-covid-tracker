33d4889893eb8cfacad33530df7d44ef80d91148
Caglar <>
2021-01-19 16:41:19 -0600

    Improve code styling

--- global_sunburst.py
+++ global_sunburst.py
@@ -24,11 +24,13 @@ df = df[['Date_reported', 'Country', 'Region', 'New_cases', 'Cumulative_cases', 
 df = df.groupby(['Country', 'Region', pd.Grouper(key='Date_reported', freq='M')]).agg({'New_cases':'sum', 'Cumulative_cases':'sum', 'Cumulative_deaths':'sum'}).reset_index()
 df['Date_reported'] = df['Date_reported'].dt.strftime('%Y-%m')
 
-import plotly.express as px
 
 df = px.data.gapminder().query("year == 2007")
-fig = px.sunburst(df, path=['continent', 'country'], values='pop',
-                  color='lifeExp', hover_data=['iso_alpha'])
+fig = px.sunburst(df,
+                  path=['continent', 'country'],
+                  values='pop',
+                  color='lifeExp',
+                  hover_data=['iso_alpha'])
 
 app.layout = html.Div([
     dcc.Graph(

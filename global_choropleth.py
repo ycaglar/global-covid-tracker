9a32a8f3e18d4bcce5b4f65cb2689614059fe631
Caglar <>
2021-01-20 09:27:37 -0600

    Improve code styling

--- global_choropleth.py
+++ global_choropleth.py
@@ -16,7 +16,7 @@ import pprint
 from dataframe import dataframe as df
 
 app = dash.Dash(
-    __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}]
+    __name__, meta_tags=[{'name': 'viewport', 'content': 'width=device-width'}]
 )
 server = app.server
 
@@ -26,23 +26,23 @@ df = df[['Date_reported', 'Country', 'Country_code', 'Region', 'New_cases', 'Cum
 df['Date_reported'] = df['Date_reported'].dt.strftime('%Y-%m-%d')
 
 fig = px.choropleth(df,
-                    locations='Country_code',
-                    color="New_cases",
-                    hover_name="Country",
-                    hover_data=['Region', 'New_cases', 'Cumulative_cases', 'Cumulative_deaths'],
-                    color_continuous_scale=px.colors.sequential.Plasma)
-fig.update_geos(projection_type="orthographic")
-fig.update_layout(height=500, margin={"r":0,"t":10,"l":0,"b":10})
+                    locations = 'Country_code',
+                    color = 'New_cases',
+                    hover_name = 'Country',
+                    hover_data = ['Region', 'New_cases', 'Cumulative_cases', 'Cumulative_deaths'],
+                    color_continuous_scale = px.colors.sequential.Plasma)
+fig.update_geos(projection_type = 'orthographic')
+fig.update_layout(height = 500, margin = {'r':0,'t':10,'l':0,'b':10})
 
 
 app.layout = html.Div([
     dcc.Graph(
-        id='corona',
-        figure=fig
+        id = 'corona',
+        figure = fig
     )
 ])
 
 
 # Main
-if __name__ == "__main__":
-    app.run_server(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug = True)

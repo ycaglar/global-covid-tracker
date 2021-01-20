dc5bd17b68c849e3f4d5cf3a57d4809b547d3ac3
Caglar <>
2021-01-19 21:08:24 -0600

    Add country code for Namibia

--- dataframe.py
+++ dataframe.py
@@ -8,13 +8,19 @@ pd.set_option('display.max_columns', None)
 # pd.set_option('display.width', None)
 # pd.set_option('display.max_colwidth', None)
 
-dataframe = pd.read_csv("https://covid19.who.int/WHO-COVID-19-global-data.csv", low_memory=False)
+dataframe = pd.read_csv('https://covid19.who.int/WHO-COVID-19-global-data.csv', low_memory=False)
 
-dataframe["Date_reported"] = pd.to_datetime(dataframe["Date_reported"])
+dataframe['Date_reported'] = pd.to_datetime(dataframe['Date_reported'])
 dataframe = dataframe[dataframe['WHO_region'] != 'Other']
 dataframe = dataframe[dataframe['New_cases'] >= 0]
 dataframe['WHO_region'] = dataframe['WHO_region'].apply(lambda abbrevation:regions[abbrevation])
 dataframe = dataframe.rename(columns={'WHO_region':'Region'})
-dataframe["Country"].replace(countries, inplace=True)
+dataframe['Country'].replace(countries, inplace=True)
+dataframe.loc[dataframe['Country'] == 'Namibia', 'Country_code'] = 'NA'
+
+# df = dataframe[['Country', 'Country_code']].drop_duplicates()
+# df.to_csv(r'~/Downloads/DataFrames/dataframe.csv', index = False)
+# breakpoint()
+
 dataframe['Country_code'].replace(iso_codes, inplace=True)
 #dataframe['Country_code'] = dataframe['Country_code'].apply(lambda code:iso_codes[code])

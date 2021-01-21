a4f75084720997c6bf8621e34ad3e3ce389c3ae2
Caglar <>
2021-01-20 21:38:26 -0600

    Simplify layout

--- global_progress.py
+++ global_progress.py
@@ -15,36 +15,27 @@ import plotly.graph_objects as go
 import pprint
 from dataframe import dataframe as df
 
-app = dash.Dash(
-    __name__, meta_tags = [{'name': 'viewport', 'content': 'width = device-width'}]
-)
-server = app.server
 
 df = df[['Date_reported', 'Country', 'Region', 'New_cases', 'Cumulative_cases', 'Cumulative_deaths']]
 df = df.groupby(['Country', 'Region', pd.Grouper(key = 'Date_reported', freq = 'M')]).agg({'New_cases':'sum', 'Cumulative_cases':'sum', 'Cumulative_deaths':'sum'}).reset_index()
 df['Date_reported'] = df['Date_reported'].dt.strftime('%Y-%m')
 
-fig = px.scatter(
-        df, x = 'Cumulative_cases', y = 'Cumulative_deaths', animation_frame = 'Date_reported',
-        animation_group = 'Country', size = 'New_cases', color = 'Region',
-        hover_name = 'Country', log_x = True, log_y = True, size_max = 100,
-        range_x = [50_000,7_000_000_00], range_y = [5_00,5_000_000_00],
-        labels = {
-                'Cumulative_cases':'Cumulative Cases',
-                'Cumulative_deaths':'Cumulative Deaths',
-                'Date_reported':'Date Reported',
-                'New_cases':'New Cases',
-                }
-        )
+fig = px.scatter(df,
+                 x = 'Cumulative_cases',
+                 y = 'Cumulative_deaths',
+                 animation_frame = 'Date_reported',
+                 animation_group = 'Country',
+                 size = 'New_cases',
+                 color = 'Region',
+                 hover_name = 'Country',
+                 log_x = True,
+                 log_y = True,
+                 size_max = 100,
+                 range_x = [50_000,7_000_000_00],
+                 range_y = [5_00,5_000_000_00],
+                 labels = {'Cumulative_cases':'Cumulative Cases',
+                           'Cumulative_deaths':'Cumulative Deaths',
+                           'Date_reported':'Date Reported',
+                           'New_cases':'New Cases',})
 
-app.layout = html.Div([
-    dcc.Graph(
-        id = 'corona',
-        figure = fig
-    )
-])
-
-
-# Main
-if __name__ == '__main__':
-    app.run_server(debug = True)
+fig.show()

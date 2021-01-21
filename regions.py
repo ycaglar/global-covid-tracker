ac67dbdc4caa52bc34cb1235c204c356cbc65489
Caglar <>
2021-01-20 22:57:52 -0600

    Improve code styling

--- regions.py
+++ regions.py
@@ -1,1 +1,6 @@
-regions = {'WPRO':'Western Pacific', 'AFRO':'Africa', 'EMRO':'Eastern Mediterranean', 'SEARO':'South-East Asia', 'EURO':'Europe', 'AMRO':'Americas'}
+regions = {'WPRO':'Western Pacific',
+           'AFRO':'Africa',
+           'EMRO':'Eastern Mediterranean',
+           'SEARO':'South-East Asia',
+           'EURO':'Europe',
+           'AMRO':'Americas'}

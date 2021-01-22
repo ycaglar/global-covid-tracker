07c0682a521ab7e999c348b57406d631e731d25e
Caglar <>
2021-01-22 16:17:42 -0600

    Add population data to the main df

--- dataframe.py
+++ dataframe.py
@@ -8,8 +8,16 @@ pd.set_option('display.max_columns', None)
 # pd.set_option('display.width', None)
 # pd.set_option('display.max_colwidth', None)
 
-dataframe = pd.read_csv('https://covid19.who.int/WHO-COVID-19-global-data.csv', low_memory = False)
+population = pd.read_csv('https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/CSV_FILES/WPP2019_TotalPopulationBySex.csv', low_memory = False)
+population = population[population['Time'] == 2020]
+population = population[['Location', 'PopTotal']].drop_duplicates()
+population = population[population['Location'] != 'Micronesia']
+population = population.astype({'PopTotal': 'int32'})
+population = population.rename(columns={'Location':'Country', 'PopTotal':'Population'})
+population['Country'].replace(countries, inplace = True)
 
+
+dataframe = pd.read_csv('https://covid19.who.int/WHO-COVID-19-global-data.csv', low_memory = False)
 dataframe['Date_reported'] = pd.to_datetime(dataframe['Date_reported'])
 dataframe = dataframe[dataframe['WHO_region'] != 'Other']
 dataframe = dataframe[dataframe['New_cases'] >= 0]
@@ -20,3 +28,4 @@ dataframe.loc[dataframe['Country'] == 'Namibia', 'Country_code'] = 'NA'
 
 #dataframe['Country_code'].replace(iso_codes, inplace = True)
 dataframe['Country_code'] = dataframe['Country_code'].apply(lambda code:iso_codes[code])
+dataframe = pd.merge(dataframe, population, on = 'Country', how = 'left')

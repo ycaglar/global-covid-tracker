96ee01123ee3d8f45c6b5241165ebc7572f50771
Caglar <>
2021-01-22 17:49:13 -0600

    Implement global treemap module

--- global_treemap.py
+++ global_treemap.py
@@ -1,12 +1,16 @@
 from dataframe import dataframe as df
 import plotly.express as px
+import numpy as np
+import pandas as pd
 
+df = df[df['Date_reported'] == pd.Timestamp.today().normalize() - pd.Timedelta(days = 1)]
+df = df[['Country', 'Region', 'Cumulative_cases', 'Population']]
 
-df = df[['Country', 'Region', 'Cumulative_cases']]
-#df = df[df['Cumulative_cases'] >= 1_000_000]
 
 fig = px.treemap(df,
                  path = [px.Constant('World'), 'Region', 'Country'],
-                 values = 'Cumulative_cases')
+                 values = 'Population',
+                 color = 'Cumulative_cases',
+                 hover_data = ['Population', 'Cumulative_cases'])
 
 fig.show()

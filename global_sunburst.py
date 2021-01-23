ae60087bcbb0e29ef8c29c170a4569093c93a6da
Caglar <>
2021-01-22 18:01:45 -0600

    Implement global sunburst module

--- global_sunburst.py
+++ global_sunburst.py
@@ -3,16 +3,15 @@ import pandas as pd
 import plotly.express as px
 from dataframe import dataframe as df
 
-df = df[['Date_reported', 'Country', 'Region', 'New_cases', 'Cumulative_cases', 'Cumulative_deaths']]
-df = df.groupby(['Country', 'Region', pd.Grouper(key = 'Date_reported', freq = 'M')]).agg({'New_cases':'sum', 'Cumulative_cases':'sum', 'Cumulative_deaths':'sum'}).reset_index()
-df['Date_reported'] = df['Date_reported'].dt.strftime('%Y-%m')
+df = df[df['Date_reported'] == pd.Timestamp.today().normalize() - pd.Timedelta(days = 1)]
+df = df[['Country', 'Region', 'Cumulative_cases', 'Population']]
+df = df[df['Population'] >= 30_000]
 
 
-df = px.data.gapminder().query('year == 2007')
 fig = px.sunburst(df,
-                  path = ['continent', 'country'],
-                  values = 'pop',
-                  color = 'lifeExp',
-                  hover_data = ['iso_alpha'])
+                  path = ['Region', 'Country'],
+                  values = 'Population',
+                  color = 'Cumulative_cases',
+                  hover_data = ['Population', 'Cumulative_cases'])
 
 fig.show()

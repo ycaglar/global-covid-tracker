621cc165f789b4e082be57373c6fc8d4b2652b03
Caglar <>
2021-01-23 16:18:11 -0600

    Change module data focus

--- global_treemap.py
+++ global_treemap.py
@@ -3,14 +3,14 @@ import plotly.express as px
 import pandas as pd
 
 df = df[df['Date_reported'] == pd.Timestamp.today().normalize() - pd.Timedelta(days = 1)]
-df = df[['Country', 'Region', 'Cumulative_cases', 'Population']]
+df = df[['Country', 'Region', 'New_cases', 'Population']]
 
 
 fig = px.treemap(df,
                  path = [px.Constant('World'), 'Region', 'Country'],
                  values = 'Population',
-                 color = 'Cumulative_cases',
-                 hover_data = ['Population', 'Cumulative_cases'],
-                 title = 'Global Distribution of Cumulative Cases by Region')
+                 color = 'New_cases',
+                 hover_data = ['Population', 'New_cases'],
+                 title = 'Global Distribution of New Cases by Region')
 
 fig.show()

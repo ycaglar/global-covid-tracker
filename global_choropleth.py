47de8eaaf14976d0d37a1ff2b8ba2677714484af
Caglar <>
2021-01-24 10:53:50 -0600

    Layout global choropleth module

--- global_choropleth.py
+++ global_choropleth.py
@@ -2,7 +2,6 @@ import pandas as pd
 import plotly.express as px
 from data_store import dataframe as df
 
-
 df = df[df['Date_reported'] == pd.Timestamp.today().normalize() - pd.Timedelta(days = 1)]
 df = df[['Date_reported', 'Country', 'Country_code', 'Region', 'New_cases', 'Cumulative_cases', 'Cumulative_deaths']]
 df['Date_reported'] = df['Date_reported'].dt.strftime('%Y-%m-%d')
@@ -12,8 +11,7 @@ fig = px.choropleth(df,
                     color = 'New_cases',
                     hover_name = 'Country',
                     hover_data = ['Region', 'New_cases', 'Cumulative_cases', 'Cumulative_deaths'],
-                    color_continuous_scale = px.colors.sequential.Plasma)
+                    color_continuous_scale = px.colors.sequential.Plasma,
+                    labels = {'New_cases':'New Cases'})
 fig.update_geos(projection_type = 'orthographic')
 fig.update_layout(height = 500, margin = {'r':0,'t':10,'l':0,'b':10})
-
-fig.show()

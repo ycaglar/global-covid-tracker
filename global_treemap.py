ace8aa5096615a2f848e088d07f69d2787cf6516
Caglar <>
2021-01-26 19:32:34 -0600

    Update global treemap color profile

--- global_treemap.py
+++ global_treemap.py
@@ -1,6 +1,7 @@
 from data_store import dataframe as df
 import plotly.express as px
 import pandas as pd
+from math import log
 
 df = df[df['Date_reported'] == pd.Timestamp.today().normalize() - pd.Timedelta(days = 1)]
 df = df[['Country', 'Region', 'New_cases', 'Population']]
@@ -9,8 +10,9 @@ df = df[['Country', 'Region', 'New_cases', 'Population']]
 fig = px.treemap(df,
                  path = [px.Constant('World'), 'Region', 'Country'],
                  values = 'Population',
-                 color = 'New_cases',
+                 color = df['New_cases'].apply(lambda new_cases:log(new_cases) if new_cases > 1 else new_cases),
+                 color_continuous_scale = 'ylgnbu',
                  hover_data = ['Population', 'New_cases'],
-                 labels = {'New_cases':'Cases'},
+                 labels = {'New_cases':'Cases', 'color':'cases'},
                  title = 'Global Distribution of New Cases by Region')
 fig.update_layout(margin = {'r':0,'t':60,'l':10,'b':10})

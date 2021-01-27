1ee44c6c8eca63920cb1f4dad8c5dd53ebe2116f
Caglar <>
2021-01-26 19:28:36 -0600

    Update global choropleth color profile

--- global_choropleth.py
+++ global_choropleth.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
 from data_store import dataframe as df
+from math import log
 
 df = df[df['Date_reported'] == pd.Timestamp.today().normalize() - pd.Timedelta(days = 1)]
 df = df[['Date_reported', 'Country', 'Country_code', 'Region', 'New_cases', 'Cumulative_cases', 'Cumulative_deaths']]
@@ -8,10 +9,10 @@ df['Date_reported'] = df['Date_reported'].dt.strftime('%Y-%m-%d')
 
 fig = px.choropleth(df,
                     locations = 'Country_code',
-                    color = 'New_cases',
+                    color = df['New_cases'].apply(lambda new_cases:log(new_cases) if new_cases > 1 else new_cases),
+                    color_continuous_scale = 'ylgnbu',
                     hover_name = 'Country',
                     hover_data = ['Region', 'New_cases', 'Cumulative_cases', 'Cumulative_deaths'],
-                    color_continuous_scale = px.colors.sequential.Plasma,
-                    labels = {'New_cases':'New Cases'})
+                    labels = {'color':'New Cases'})
 fig.update_geos(projection_type = 'orthographic')
 fig.update_layout(margin = {'r':0,'t':10,'l':0,'b':10})

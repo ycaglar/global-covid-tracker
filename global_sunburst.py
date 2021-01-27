c56d2a237f66d1842fc8c329f3ae9d7251f8ed2c
Caglar <>
2021-01-26 21:01:40 -0600

    Update global sunburst color profile

--- global_sunburst.py
+++ global_sunburst.py
@@ -11,6 +11,7 @@ fig = px.sunburst(df,
                   path = ['Region', 'Country'],
                   values = 'Population',
                   color = 'Cumulative_cases',
+                  color_continuous_scale = 'ylgnbu',
                   hover_data = ['Population', 'Cumulative_cases'],
                   labels = {'Cumulative_cases':'Cases'},
                   title = 'Global Distribution of Cumulative Cases by Region')

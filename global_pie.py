4de77698c37af0058d536489b56ae6de312fca15
Caglar <>
2021-01-27 09:18:22 -0600

    Improve line graph color selection

--- global_pie.py
+++ global_pie.py
@@ -8,6 +8,6 @@ df = df[df['Cumulative_cases'] >= 1_000_000]
 fig = px.pie(df,
              values = 'Cumulative_cases',
              names = 'Country',
-             color_discrete_sequence = colors.YlGnBu[::-1],
+             color_discrete_sequence = colors.YlGnBu_e[::-1],
              title = 'Major Outbrakes Around the World')
 fig.update_layout(margin = {'r':0,'t':60,'l':10,'b':10})
--- local_line.py
+++ local_line.py
@@ -1,5 +1,6 @@
 import plotly.express as px
 from data_store import dataframe
+import colors
 
 def fig(country):
     df = dataframe
@@ -8,12 +9,7 @@ def fig(country):
     fig = px.line(df,
                   x = 'Date_reported',
                   y = ['New_cases', 'New_deaths'],
-                   color_discrete_sequence = ['#c7e9b4',
-                                              '#7fcdbb',
-                                              '#41b6c4',
-                                              '#1d91c0',
-                                              '#225ea8',
-                                              '#253494'],
+                  color_discrete_sequence = [colors.YlGnBu[4], colors.YlGnBu[6]],
                   labels = {'Date_reported':'Date Reported', 'value':'New Cases & New Deaths', 'variable':'Variables'},
                   title = f'{country} Status Report')
     fig.update_layout(margin = {'r':0,'t':60,'l':10,'b':10})

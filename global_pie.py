3c9d55ea1ef19de7241e131ca5cd83157da1c849
Caglar <>
2021-01-26 21:06:23 -0600

    Update global pie color profile

--- global_pie.py
+++ global_pie.py
@@ -8,5 +8,11 @@ df = df[df['Cumulative_cases'] >= 1_000_000]
 fig = px.pie(df,
              values = 'Cumulative_cases',
              names = 'Country',
+             color_discrete_sequence = ['#c7e9b4',
+                                        '#7fcdbb',
+                                        '#41b6c4',
+                                        '#1d91c0',
+                                        '#225ea8',
+                                        '#253494'],
              title = 'Major Outbrakes Around the World')
 fig.update_layout(margin = {'r':0,'t':60,'l':10,'b':10})

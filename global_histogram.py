45277ef13bda2db5932027d396cb8041c9cf1876
Caglar <>
2021-01-26 20:55:37 -0600

    Update global histogram color profile

--- global_histogram.py
+++ global_histogram.py
@@ -1,13 +1,18 @@
 import plotly.express as px
 from data_store import dataframe as df
 
-
 df = df[df['New_cases'] > 0]
 
 fig = px.histogram(df,
                    x = 'Date_reported',
                    y = 'New_cases',
                    color = 'Region',
+                   color_discrete_sequence = ['#c7e9b4',
+                                              '#7fcdbb',
+                                              '#41b6c4',
+                                              '#1d91c0',
+                                              '#225ea8',
+                                              '#253494'],
                    hover_data = df.columns,
                    barmode = 'overlay',
                    labels = {'New_cases':'New Cases', 'Date_reported':'Date Reported', 'WHO_region':'Region'},

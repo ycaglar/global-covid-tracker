670f30e31a5e82c2af3bb244b5458e7226682d72
Caglar <mail@caalar.com>
2021-01-30 04:11:15 -0600

    Fix population display

--- app.py
+++ app.py
@@ -229,10 +229,10 @@ app.layout = html.Div(
     Input(component_id = 'country_selector', component_property = 'value')
 )
 def update_output_div(input_value):
-    selected_df = df[df['Country'] == input_value].tail(1)
+    selected_df = df[df['Country'] == input_value].tail(1).astype({'Population': 'str'})
     return local_line(input_value),\
            app.get_asset_url('Flags_S/'+input_value+'.svg'),\
-           selected_df['Population'],\
+           selected_df['Population']+'000',\
            selected_df['Cumulative_cases'],\
            selected_df['Cumulative_deaths']
 

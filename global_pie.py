0ac0d3d1c91e3aace71e90cc99a4af7bdd806f34
Caglar <2038887+ycaglar@users.noreply.github.com>
2021-03-01 19:13:13 -0600

    Improve global_pie module hover data

--- global_pie.py
+++ global_pie.py
@@ -8,11 +8,13 @@ df = df.groupby('Country').tail(1)
 
 #df = df[df['Cumulative_cases'] / int(gdf['Cumulative_cases']) >= 0.011]
 df.sort_values(by = ['Cumulative_cases'], inplace = True, ascending = False)
+df = df[:19]
 
-fig = px.pie(df[:19],
+fig = px.pie(df,
              values = 'Cumulative_cases',
              names = 'Country',
              color_discrete_sequence = colors.YlGnBu_e[::-1],
+             labels = {'Cumulative_cases':'Cumulative Cases'},
              title = 'Major Outbrakes Around the World')
 fig.update_layout(margin = {'r':0,'t':60,'l':10,'b':10})
 fig.update_traces(textposition = 'inside', textinfo = 'percent+label')

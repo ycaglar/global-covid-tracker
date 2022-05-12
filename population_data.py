e50115e4aefdc602638efa34542d88a81e4c9db2
Caglar <2038887+ycaglar@users.noreply.github.com>
2022-05-11 21:47:47 -0500

    Code cleanup

--- population_data.py
+++ population_data.py
@@ -9,9 +9,11 @@ United Nation's Database and cleaned/organized to fit the use case of the app.
 import pandas as pd
 from country_names import names
 
-df = pd.read_csv('https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/CSV_FILES/WPP2019_TotalPopulationBySex.csv',
-                 low_memory = False,
-                 usecols = ['Location', 'Time', 'PopTotal'])
+df = pd.read_csv('https://population.un.org/wpp/Download/Files/\
+                  1_Indicators%20(Standard)/CSV_FILES/\
+                  WPP2019_TotalPopulationBySex.csv',
+                 low_memory=False,
+                 usecols=['Location', 'Time', 'PopTotal'])
 
 df = df[df['Time'] == 2020]
 df = df[['Location', 'PopTotal']].drop_duplicates()
@@ -27,5 +29,5 @@ df = df.astype({'PopTotal': 'int64'})
 # locale.setlocale(locale.LC_ALL, '')
 # df['PopTotal'] = df['PopTotal'].apply(lambda value:f'{value:n}')
 
-df = df.rename(columns={'Location':'Country', 'PopTotal':'Population'})
-df['Country'].replace(names, inplace = True)
+df = df.rename(columns={'Location': 'Country', 'PopTotal': 'Population'})
+df['Country'].replace(names, inplace=True)
